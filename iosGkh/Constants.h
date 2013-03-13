c1db9271c8c2c874a6e1b426741e7bc8946c9f0b
tweak <tweakon@gmail.com>
2013-03-13 15:25:14 +0400

    Constants.h

---
+++ iosGkh/Constants.h
@@ -0,0 +1,14 @@
+//
+//  Constants.h
+//  iosGkh
+//
+//  Created by Evgeniy Sorokin on 13.03.13.
+//  Copyright (c) 2013 Prosoftlab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Constants : NSObject
+FOUNDATION_EXPORT NSString *const BarPlot;
+FOUNDATION_EXPORT NSString *const PieChart;
+@end
---
+++ iosGkh/Constants.m
@@ -0,0 +1,14 @@
+//
+//  Constants.m
+//  iosGkh
+//
+//  Created by Evgeniy Sorokin on 13.03.13.
+//  Copyright (c) 2013 Prosoftlab. All rights reserved.
+//
+
+#import "Constants.h"
+
+@implementation Constants
+NSString *const BarPlot  = @"BAR_PLOT";
+NSString *const PieChart = @"PIE_CHART";
+@end
