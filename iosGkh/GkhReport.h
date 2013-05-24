7b231377689e43f5a16cd26b51f1f54f7f40abc6
TheTweak <tweakon@gmail.com>
2013-05-24 12:50:00 +0400

    GkhReport class

---
+++ iosGkh/GkhReport.h
@@ -0,0 +1,56 @@
+//
+//  GkhReport.h
+//  iosGkh
+//
+//  Отчет, то что загружается в таблицу на главном экране пользователя с ролью УК.
+//
+//  Created by Sorokin E on 24.05.13.
+//  Copyright (c) 2013 Prosoftlab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+// Типы графиков
+typedef enum {
+    GkhPlotTypeBar // гистограмма
+    GkhPlotTypeXY
+    GkhPlotTypeCircle
+} GkhPlotType;
+
+@interface GkhReport : NSObject
+
+@property(readonly) NSString *id;
+@property(readonly) NSString *name;
+@property(readonly) NSString *description;
+@property(readonly) NSArray *additionalRepresentationArray;
+@property(readonly) GkhPlotType plotType;
+
+@end
+
+typedef enum {
+    GkhRepresentationTypeTable
+} GkhRepresentationType;
+
+// Описывает дополнительные представления отчета, в дополнение к графику. Они отображаются
+// в scrollView вместе с графиком. Пока поддерживается только таблица.
+@interface GkhRepresentation : NSObject
+
+@property(readonly) NSString *id;
+@property(readonly) GkhRepresentationType type;
+
+@end
+
+// Тип контрола для параметра
+typedef enum {
+    GkhInputRepresentationTypeCombo // UITextField с выбором в виде UIActionSheet
+} GkhInputRepresentationType;
+
+// Типы входных параметров
+@interface GkhInputType : NSObject
+
+@property(readonly) NSString *id
+@property(readonly) NSString *description;
+@property(readonly) GkhInputRepresentationType representationType;
+@property id value; // значение параметра, которое посылается на сервер
+
+@end
---
+++ iosGkh/GkhReport.m
@@ -0,0 +1,14 @@
+//
+//  GkhReport.m
+//  iosGkh
+//
+//
+//  Created by Sorokin E on 24.05.13.
+//  Copyright (c) 2013 Prosoftlab. All rights reserved.
+//
+
+#import "GkhReport.h"
+
+@implementation GkhReport
+
+@end
