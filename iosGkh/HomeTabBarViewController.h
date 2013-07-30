26f320af54212b09fcd8e791f5d91a883a277580
TheTweak <tweakon@gmail.com>
2013-07-30 18:25:19 +0400

    new icon + "Back" button fixed

---
+++ iosGkh/HomeTabBarViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeTabBarViewController.h
+//  iosGkh
+//
+//  Created by Sorokin E on 30.07.13.
+//  Copyright (c) 2013 Prosoftlab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeTabBarViewController : UITabBarController <UITabBarControllerDelegate>
+
+@end
---
+++ iosGkh/HomeTabBarViewController.m
@@ -0,0 +1,34 @@
+//
+//  HomeTabBarViewController.m
+//  iosGkh
+//
+//  Created by Sorokin E on 30.07.13.
+//  Copyright (c) 2013 Prosoftlab. All rights reserved.
+//
+
+#import "HomeTabBarViewController.h"
+
+@interface HomeTabBarViewController ()
+
+@end
+
+@implementation HomeTabBarViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	self.delegate = self;
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    self.title = self.selectedViewController.title;
+}
+
+-(void)tabBarController:(UITabBarController *)tabBarController didSelectViewController:(UIViewController *)viewController
+{
+    self.title = viewController.title;
+}
+
+@end
